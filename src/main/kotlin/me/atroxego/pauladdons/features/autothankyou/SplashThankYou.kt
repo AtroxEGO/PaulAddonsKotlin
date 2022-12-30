1f025b5b059b687d4fa13afebb43874d9d0ccb9a
Pablo <pawel.polakiewicz1@gmail.com>
2022-12-30 16:04:47 +0100

    bug fix with auto thank you

--- src/main/kotlin/me/atroxego/pauladdons/features/autothankyou/SplashThankYou.kt
+++ src/main/kotlin/me/atroxego/pauladdons/features/autothankyou/SplashThankYou.kt
@@ -2,6 +2,7 @@ package me.atroxego.pauladdons.features.autothankyou
 
 import PaulAddons.Companion.mc
 import me.atroxego.pauladdons.config.Config
+import me.atroxego.pauladdons.utils.Utils.stripColor
 import net.minecraftforge.client.event.ClientChatReceivedEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
@@ -11,9 +12,9 @@ object SplashThankYou {
     fun onSplash(event: ClientChatReceivedEvent){
         if(!Config.autoThankYou) return
         if(System.currentTimeMillis() - lastTimeThanked < 20000) return
-        val message = event.message.unformattedText
-        if (message.startsWith("BUFF! You were splashed by ")){
-            val splasher = message.removePrefix("BUFF! You were splashed by ").split(" ")[0]
+        val message = event.message.unformattedText.stripColor()
+        if(message.startsWith("BUFF! You were splashed by",true)){
+            val splasher = message.removePrefix("BUFF! You were splashed by ").split(" ")[0].stripColor()
             val messageToSend = Config.thankYouMessage.replace("[IGN]",splasher)
             mc.thePlayer.sendChatMessage(messageToSend)
             lastTimeThanked = System.currentTimeMillis()
